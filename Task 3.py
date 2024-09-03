344d75c7e545ea82455278870f18ddd83f25335a
bykva98 <bykva.98@mail.ru>
2024-09-04 01:15:04 +0700

    Add files via upload

---
+++ Task 3.py
@@ -0,0 +1,5 @@
+sum_homework = 12
+sum_hours = 1.5
+name = 'Python'
+#time = sum_hours / sum_homework = 0.125
+print("Курс: ", name, ",", "всего задач: ", sum_homework, ",", "затрачено часов: ", sum_hours, ",", "среднее время выполнения", (sum_hours / sum_homework), "часа.")
